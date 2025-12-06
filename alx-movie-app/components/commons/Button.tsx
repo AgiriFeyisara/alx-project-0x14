15597a4db5f205b5ebb56bede179ae5ba2e75308
Feyisara Agiri <mohrenny8@gmail.com>
2025-12-06 07:36:07 +0100

    Created a next directory in my project

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,4 @@
+const Button: React.FC = () => {
+  return <div></div>;
+};
+export default Button;
---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,4 @@
+const MovieCard: React.FC = () => {
+  return <div></div>;
+};
+export default MovieCard;
